b7688e957ca41c6d7e9133b866cc33491a6b5611
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-05-07 22:43:16 -0600

    logic in place needs work

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -17,8 +17,37 @@ app.get("/api/friends", function (req, res) {
 //      (same question = same index)
 // Add differences between and calculate "totalDifference" 
 //      (use absolute value of numbers!)
+// MOST LIKELY ISSUE: 
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
 
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body.scores);
+        var user = req.body;
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
 
+        var bestMatch = 0;
+        var minDiff = 40;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var totalDiff = 0;
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+                var diff = Math.abs(user.scores[j] - friendsData[i].scores[j]);
+                totalDiff += diff;
+            }
+            if (totalDiff < minDiff) {
+                bestMatch = i;
+                minDiff = totalDiff;
+            }
+        }
+        friendsData.push(user);
+        res.json(friendsData[bestMatch]);
+    });
+};
 // display result as modal pop-up: name and pic of closest match
 
 // **Potential problems (time pending): 
